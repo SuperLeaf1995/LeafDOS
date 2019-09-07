e14acb77751e72e72141ba76546658adc47b7593
Jesus Antonio Diaz <jesusantonio30122016@gmail.com>
2019-09-06 22:00:40 -0500

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,12 +1,22 @@
 ; Use the -i option in NASM
 %include "drivers/text.asm"
+%include "drivers/key.asm"
+[ORG 0x7C00]
+[BITS 16] ; We want 16 bits, deal with it
+  xor ax, ax
+  
   ;segment * 16 + offset = address
-  mov ax, 0x07C0
   mov ds, ax
+  mov es, ax
+  mov ss, ax
+  mov sp, 0x7C00
   cli ; Flush registers and stuff, basicaly we are all clear
+  
   ; A welcoming message
   mov si, boot_msg1
   call print_text
+  
+  jmp boot
 boot:
   jmp $ ; Loop forever
   
