f70b940c3005bf1f9b09edb7226d24d221e82ebe
Superleaf1995 <jesusantonio30122016@gmail.com>
2020-04-07 09:59:12 +0000

    Giving raw arguments instead of parsed ones!

--- src/programs/command.asm
+++ src/programs/command.asm
@@ -81,7 +81,7 @@ start:
 	mov si, kernel_program_start
 	call near _printf
 	
-	mov si, kernel_buffer.fat12 ; Arguments are given in SI
+	mov si, kernel_buffer.keyboard ; Arguments are given in SI
 	call SEG_PROGRAM
 	
 	mov si, kernel_program_end
